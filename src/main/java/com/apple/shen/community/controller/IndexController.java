0d05098682d8c08c0155d84de7eb4cdb6ee3ec06
apple302 <shenjia2010@163.com>
2019-11-02 00:33:19 +0800

    add split page support

--- src/main/java/com/apple/shen/community/controller/IndexController.java
+++ src/main/java/com/apple/shen/community/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.apple.shen.community.controller;
 
+import com.apple.shen.community.dto.PaginationDTO;
 import com.apple.shen.community.dto.QuestionDTO;
 import com.apple.shen.community.mapper.UserMapper;
 import com.apple.shen.community.model.User;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -24,6 +26,8 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
+                        @RequestParam(name = "page",defaultValue = "1") Integer page,
+                        @RequestParam(name = "size",defaultValue = "2") Integer size,
                         Model model){
         Cookie[] cookies = request.getCookies();
         if(cookies != null && cookies.length !=0) {
@@ -39,10 +43,8 @@ public class IndexController {
             }
         }
 
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
-
-
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 }
--- src/main/java/com/apple/shen/community/service/QuestionService.java
+++ src/main/java/com/apple/shen/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.apple.shen.community.service;
 
+import com.apple.shen.community.dto.PaginationDTO;
 import com.apple.shen.community.dto.QuestionDTO;
 import com.apple.shen.community.mapper.QuestionMapper;
 import com.apple.shen.community.mapper.UserMapper;
@@ -21,8 +22,23 @@ public class QuestionService {
     @Autowired
     private QuestionMapper questionMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questionList = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+
+        //获取问题总数量
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount, page, size);
+
+        if(page < 1){
+            page = 1;
+        }
+        if(page > paginationDTO.getTotalPage()){
+            page = paginationDTO.getTotalPage();
+        }
+
+        // 5*(n-1)：每页5条数据
+        Integer offset = size * (page -1);
+        List<Question> questionList = questionMapper.list(offset, size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
         for (Question question : questionList) {
             User user = userMapper.findById(question.getCreator());
@@ -31,6 +47,9 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+
+
+        return paginationDTO;
     }
 }
